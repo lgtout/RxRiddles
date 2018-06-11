8870fe012056e957b9bc7c9c5e87d60677e4ddd0
lgtout <lagos.tout@gmail.com>
2018-06-10 23:17:39 -0700

    Solutions to riddles 16-18

--- src/main/kotlin/com/vanniktech/rxriddles/Riddle16.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle16.kt
@@ -11,6 +11,8 @@ object Riddle16 {
    * Use case: The [first] Observable is a TextField and you want to issue a network request while disposing the old requests in case the user has typed something new.
    */
   fun solve(first: Observable<String>, function: (String) -> Single<Int>): Observable<Int> {
-    TODO()
+      return first.switchMapSingle { string: String ->
+          function(string)
+      }
   }
 }
