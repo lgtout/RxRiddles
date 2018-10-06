0c5df94bebe124bd0e0ce97a966c3570a3416d53
lgtout <lagos.tout@gmail.com>
2018-10-06 11:47:17 -0700

    Solutions to riddles 19-22

--- src/main/kotlin/com/vanniktech/rxriddles/Riddle19.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle19.kt
@@ -10,7 +10,12 @@ object Riddle19 {
    * Use case: Transform any listener into an Observable.
    */
   fun solve(interaction: Interaction): Observable<Int> {
-    TODO()
+    return Observable.create<Int> { emitter ->
+      interaction.listener = {
+        emitter.onNext(it)
+      }
+      emitter.setCancellable { interaction.listener = null }
+    }
   }
 
   interface Interaction {
